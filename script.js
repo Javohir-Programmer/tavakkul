ca7abcdd827a00ff1068a0b2083ab79b390fb18f
Javohir <jabduraimov2002@gmail.com>
2024-05-01 10:29:39 +0500

    yangi

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let sun = document.querySelector('#sun');
 let moon = document.querySelector('#moon');
 let body = document.querySelector('body');
+let menuIcon = document.querySelector('#menu');
+let menu_box = document.querySelector('.menu-box');
+let x = document.querySelector('#x');
+
+// dark mood
 
 sun.addEventListener("click", () => {
     body.classList.add("darkLight");
@@ -11,4 +16,14 @@ moon.addEventListener("click", () => {
     body.classList.remove("darkLight");
     moon.classList.remove("block");
     sun.classList.remove("none");
+});
+
+// nav bar
+
+menuIcon.addEventListener("click", () => {
+    menu_box.classList.add("left");
+});
+
+x.addEventListener("click", () => {
+    menu_box.classList.remove("left");
 });
